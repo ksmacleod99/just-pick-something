eeeb139c1f6b9b875f981611038175b5e10bc030
K MacLeod <ksmacleod99@gmail.com>
2021-05-02 18:47:56 -0400

    added vue material design

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 
 //createApp(App).use(router).mount('#app')
